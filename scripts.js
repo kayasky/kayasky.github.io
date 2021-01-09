b83cd03d830efaf6bacb9dfa1197e9b2abe5e9a9
Kaya Jhuti <itskayaj@gmail.com>
2021-01-08 22:21:44 -0500

    Refine animation logic

--- scripts.js
+++ scripts.js
@@ -1,15 +1,21 @@
 const html = document.documentElement;
 const canvas = document.getElementById("hero-lightpass");
-const introHeader = document.getElementById("intro__header");
+const section1Header = document.getElementById("section1Header");
+const section2Header = document.getElementById("section2Header");
+let section2HeaderOpacity = 0;
+let section2HeaderTransform = 0;
 const section1 = document.getElementById("section1");
 const section2 = document.getElementById("section2");
 const section2InitialTop = section2.getBoundingClientRect().top;
 const context = canvas.getContext("2d");
 let stopAt = 0;
+let last_known_scroll_position = 0;
+let ticking = false;
+
 
 const frameCount = 148;
-const introHeaderMaxFontSize = 100;
-const introHeaderFontSize = parseInt(window.getComputedStyle(introHeader).fontSize, 10);
+const section1HeaderMaxFontSize = 100;
+const section1HeaderFontSize = parseInt(window.getComputedStyle(section1Header).fontSize, 10);
 const currentFrame = index => (
   `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
 )
@@ -34,7 +40,17 @@ const updateImage = index => {
   context.drawImage(img, 0, 0);
 }
 
-window.addEventListener('scroll', () => {
+document.addEventListener('scroll', function () {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    handleScrollEvent();
+
+    ticking = true;
+  }
+});
+
+const handleScrollEvent = () => {
   const scrollTop = html.scrollTop;
   const maxScrollTop = html.scrollHeight - window.innerHeight;
   const scrollFraction = scrollTop / maxScrollTop;
@@ -44,23 +60,29 @@ window.addEventListener('scroll', () => {
     Math.ceil(scrollFraction * frameCount)
   );
 
-  //requestAnimationFrame(() => updateImage(frameIndex + 1))
-  introHeader.style.fontSize = `${introHeaderFontSize + Math.min(frameIndex * 1.5, introHeaderMaxFontSize)}px`;
-  console.log(section2InitialTop, window.pageYOffset, parseInt(window.getComputedStyle(section2).height, 10));
+  requestAnimationFrame(() => { updateImage(frameIndex + 1); ticking = false;})
+  section1Header.style.fontSize = `${section1HeaderFontSize + Math.min(frameIndex * 1.5, section1HeaderMaxFontSize)}px`;
   if (window.pageYOffset < section2InitialTop) {
-
+    section2HeaderOpacity = 0;
   } else if (window.pageYOffset > section2InitialTop && window.pageYOffset < section2InitialTop + parseInt(window.getComputedStyle(section2).height, 10)) {
     section2.style.top = `${window.pageYOffset}px`;
+    section2HeaderOpacity = ((window.pageYOffset - section2InitialTop) / parseInt(window.getComputedStyle(section2).height, 10));
+
+    section2HeaderTransform = section2HeaderOpacity * 100;
   } else if (window.pageYOffset > section2InitialTop + parseInt(window.getComputedStyle(section2).height, 10)) {
     if (!stopAt) {
       stopAt = window.pageYOffset;
       section2.style.top = `${stopAt}px`;
     }
+    section2HeaderOpacity = 1;
 
   } else {
     section2.style.top = '';
   }
 
-});
+  section2Header.style.opacity = section2HeaderOpacity;
+  section2Header.style.transform = `translateY(calc(50vh - ${section2HeaderTransform}px))`;
+
+}
 
 preloadImages()
