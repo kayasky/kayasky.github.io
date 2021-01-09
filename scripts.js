86e34a526cab9e8b5565e781fc751197edccb842
Kaya Jhuti <itskayaj@gmail.com>
2021-01-08 21:03:42 -0500

    Add animation for section 2

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 const html = document.documentElement;
 const canvas = document.getElementById("hero-lightpass");
 const introHeader = document.getElementById("intro__header");
+const section1 = document.getElementById("section1");
+const section2 = document.getElementById("section2");
+const section2InitialTop = section2.getBoundingClientRect().top;
 const context = canvas.getContext("2d");
+let stopAt = 0;
 
 const frameCount = 148;
 const introHeaderMaxFontSize = 100;
@@ -41,7 +45,22 @@ window.addEventListener('scroll', () => {
   );
 
   //requestAnimationFrame(() => updateImage(frameIndex + 1))
-  introHeader.style.fontSize = `${introHeaderFontSize + Math.min(frameIndex, introHeaderMaxFontSize)}px`;
+  introHeader.style.fontSize = `${introHeaderFontSize + Math.min(frameIndex * 1.5, introHeaderMaxFontSize)}px`;
+  console.log(section2InitialTop, window.pageYOffset, parseInt(window.getComputedStyle(section2).height, 10));
+  if (window.pageYOffset < section2InitialTop) {
+
+  } else if (window.pageYOffset > section2InitialTop && window.pageYOffset < section2InitialTop + parseInt(window.getComputedStyle(section2).height, 10)) {
+    section2.style.top = `${window.pageYOffset}px`;
+  } else if (window.pageYOffset > section2InitialTop + parseInt(window.getComputedStyle(section2).height, 10)) {
+    if (!stopAt) {
+      stopAt = window.pageYOffset;
+      section2.style.top = `${stopAt}px`;
+    }
+
+  } else {
+    section2.style.top = '';
+  }
+
 });
 
 preloadImages()
